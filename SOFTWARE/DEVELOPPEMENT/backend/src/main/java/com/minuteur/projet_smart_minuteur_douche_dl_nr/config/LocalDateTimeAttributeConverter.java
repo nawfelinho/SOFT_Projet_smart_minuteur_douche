f14f3c1103b28d1ccdfc9f531a2bb3badb75e8a2
ElDLOR <163985620+ElDLOR@users.noreply.github.com>
2025-05-25 22:08:56 +0200

    MàJ DCD/DFD + Test méthode POST/GET/PATCH + Ajout classe LocalDateTimeAttributeConverter

---
+++ SOFTWARE/DEVELOPPEMENT/backend/src/main/java/com/minuteur/projet_smart_minuteur_douche_dl_nr/config/LocalDateTimeAttributeConverter.java
@@ -0,0 +1,23 @@
+package com.minuteur.projet_smart_minuteur_douche_dl_nr.config;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Converter(autoApply = true)
+public class LocalDateTimeAttributeConverter implements AttributeConverter<LocalDateTime, String> {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+    @Override
+    public String convertToDatabaseColumn(LocalDateTime attribute) {
+        return attribute != null ? attribute.format(FORMATTER) : null;
+    }
+
+    @Override
+    public LocalDateTime convertToEntityAttribute(String dbData) {
+        return dbData != null ? LocalDateTime.parse(dbData, FORMATTER) : null;
+    }
+}
